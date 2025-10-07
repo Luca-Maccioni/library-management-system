1590a1baea393f512e9725432faed26af341ab23
Luca-Maccioni <luca.maccioni@icloud.com>
2025-10-07 09:28:59 +0200

    add : Footer directory and Navbar directory + mv navbar and footer files

---
+++ frontend/library-frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,82 @@
+import './Footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'; // Icônes de marque (fab)
+import { faRss } from '@fortawesome/free-solid-svg-icons'; // Icône solide (fas)
+
+
+function Footer () {
+    return (
+        <>
+        
+        <footer className='footer'>
+            <div className='columns'>
+                <div className='footer-column'>
+                    <h1 className='column-title'>Library</h1>
+                    <ul className='column-list'>
+                        <li><a href="/">Pricing</a></li>
+                         <li><a href="/">Updates</a></li>
+                         <li><a href="/">Beta</a></li>
+                         <li><a href="/">Newsletter</a></li>
+                         <li><a href="/">Collaborations</a></li>
+                    </ul>
+                </div>
+                <div className='footer-column'>
+                    <h1 className='column-title'>Product</h1>
+                    <ul className='column-list'>
+                        <li><a href="/">Business</a></li>
+                        <li><a href="/">Designers</a></li>
+                        <li><a href="/">Classrooms</a></li>
+                        <li><a href="/">Newcomers</a></li>
+                    </ul>
+                </div>
+                <div className='footer-column'>
+                    <h1 className='column-title'>Learning</h1>
+                    <ul className='column-list'>
+                        <li><a href="/">Learn Hub</a></li>
+                        <li><a href="/">Manuals</a></li>
+                        <li><a href="/">Tutorials</a></li>
+                        <li><a href="/">Communities</a></li>
+                    </ul>
+                </div>
+                <div className='footer-column'>
+                    <h1 className='column-title'>Ressources</h1>
+                    <ul className='column-list'>
+                        <li><a href="/">Tutorials</a></li>
+                        <li><a href="/">Editorials</a></li>
+                        <li><a href="/">Product</a></li>
+                        <li><a href="/">Newsroom</a></li>
+                    </ul>
+                </div>
+                <div className='footer-column'>
+                    <h1 className='column-title'>About</h1>
+                    <ul className='column-list'>
+                        <li><a href="/">Company</a></li>
+                        <li><a href="/">Careers</a></li>
+                        <li><a href="/">Legal</a></li>
+                        <li><a href="/">Help</a></li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div className="footer-bottom">
+                <div className="footer-logo">
+                    <span>LIBRARY</span> 
+                    </div>
+                    
+                    <p className="footer-copyright">
+                        © 2025 Library. All rights reserved.
+                    </p>
+                    
+            <div className="footer-socials">
+                <a href="https://facebook.com" aria-label="Facebook"><FontAwesomeIcon icon={faFacebookF} /></a>
+                <a href="https://twitter.com" aria-label="Twitter"><FontAwesomeIcon icon={faTwitter} /></a>
+                <a href="https://instagram.com" aria-label="Instagram"><FontAwesomeIcon icon={faInstagram} /></a>
+                <a href="https://rss.com" aria-label="RSS Feed"><FontAwesomeIcon icon={faRss} /></a>
+            </div>
+      </div>
+    </footer>
+    </>
+    )
+}
+
+export default Footer
