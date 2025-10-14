b223abb8c8cf699951da5837fe03ea915db57c29
Luca-Maccioni <luca.maccioni@icloud.com>
2025-10-14 09:00:01 +0200

    add : backend user sign up

--- frontend/library-frontend/src/components/Auth/SignUp.jsx
+++ frontend/library-frontend/src/components/Auth/SignUp.jsx
@@ -1,9 +1,22 @@
-import './SignUp.css'
+import { useState } from 'react';
+import './SignUp.css';
 
-function SignUp () {
+function SignUp() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async () => {
+        const response = await fetch("http://localhost:8080/api/auth/signup", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ username, email, password })
+        });
+        const data = await response.text();
+        console.log(data);
+    }
+
     return (
-        <>
-        
         <div className="page-container">
             <div className="header">
                 <div className="text">Sign Up</div>
@@ -11,13 +24,13 @@ function SignUp () {
 
             <div className="inputs">
                 <div className="input">
-                    <input type="text" placeholder="Username" />
+                    <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
                 </div>
                 <div className="input">
-                    <input type="text" placeholder="Email Adress" />
+                    <input type="text" placeholder="Email Address" value={email} onChange={e => setEmail(e.target.value)} />
                 </div>
                 <div className="input">
-                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
                 </div>
             </div>
 
@@ -26,13 +39,10 @@ function SignUp () {
             </div>
 
             <div className="submit-container">
-                <div className="submit">Sign Up</div>
+                <div className="submit" onClick={handleSubmit}>Sign Up</div>
             </div>
         </div>
-
-
-        </>
     )
 }
 
-export default SignUp
+export default SignUp;
